0245aaab07765039c7dcbca8f2a489083eb0d853
1BM18CS114 <54112164+1BM18CS114@users.noreply.github.com>
2019-09-10 22:04:52 +0530

    Add files via upload

---
+++ BOTTOM.C
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<conio.h>
+
+int push(int *top, int obj[]);
+void sort(int top, int obj[]);
+void main(){
+	int obj[10], top = -1, n, i;
+	clrscr();
+	printf("Enter number of values in stack: ");
+	scanf("%d", &n);
+	printf("Enter values for stack(bottom to top)\n");
+	for(i=0;i<n;i++)
+		obj[top] = push(&top, obj);
+	sort(top, obj);
+	getch();
+	}
+
+int push(int *top, int obj[]){
+	int x;
+	if(*top >= 10){
+		printf("Sorry stack overflow\n\n\n");
+		return obj[*top];
+		}
+
+	else{
+		printf("Enter value to be added:\n");
+		scanf("%d", &x);
+		printf("Value added\n\n\n");
+		(*top)++;
+		return x;
+		}
+	}
+
+void sort(int top, int obj[]){
+	int i, j, temp;
+	temp = obj[0];
+	for(i = 1; i <= top; i++){
+		if(obj[i] <= temp){
+			temp = obj[i];
+			for(j = i-1; j >= 0; j--){
+				//printf("\n%d %d\n", obj[j+1], obj[j]);
+				obj[j+1] = obj[j];
+				}
+			obj[0] = temp;
+			}
+		}
+	for(i=top; i>=0; i--)
+		printf("%d\n", obj[i]);
+
+	}
