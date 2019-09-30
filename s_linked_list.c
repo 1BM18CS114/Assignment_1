549e118fb8e107fe8b2072eb20862302acbc62db
1BM18CS114 <54112164+1BM18CS114@users.noreply.github.com>
2019-09-30 15:44:59 +0530

    Update s_linked_list.c

--- s_linked_list.c
+++ s_linked_list.c
@@ -12,17 +12,25 @@ node get_node();
 node ins_f(node first, int val);
 node ins_b(node first, int val);
 void display(node first);
+node del_f(node first);
+node del_b(node first);
+node ins_p(node first, int pos, int val);
+node del_p(node first, int pos);
 
 node first = NULL;
 
 int main(){
-	int i, x, val;
+	int i, x, val, pos;
 	
-	while(x != 4){
+	while(x != 8){
 		printf("1: Enter val from front\n");
 		printf("2: Enter val from back\n");
 		printf("3: Display\n");
-		printf("4: Exit\n");
+		printf("4: Delete val from front\n");
+		printf("5: Delete val from back\n");
+		printf("6: Enter val from pos\n");
+		printf("7: Delete val from pos\n");
+		printf("8: Exit\n");
 		scanf("%d", &x);
 			switch(x){
 			case 1:
@@ -39,6 +47,28 @@ int main(){
 				
 			case 3:
 				display(first);
+				break;
+				
+			case 4:
+				first = del_f(first);
+				break;
+				
+			case 5:
+				first = del_b(first);
+				break;
+			
+			case 6:
+				printf("Enter value from back: ");
+				scanf("%d", &val);
+				printf("Enter position: ");
+				scanf("%d", &pos);
+				first = ins_p(first, pos, val);
+				break;
+			
+			case 7:
+				printf("Enter position: ");
+				scanf("%d", &pos);
+				first = del_p(first, pos);
 				break;
 			}
 		}
@@ -97,7 +127,92 @@ void display(node first){
 		}
 	}
 
-		
-		
-	 
-		
+node del_f(node first){
+	node temp;
+	int val;
+	if(first == NULL){
+		val = -1;
+		printf("Linked list Empty");
+		}
+	else{
+		val = first->val;
+		temp = first;
+		first = first->next;
+		free(temp);
+		printf("Value from front %d deleted\n", val);
+		}
+	return first;
+	}
+	
+node del_b(node first){
+	node temp, temp_b;
+	int val;
+	if(first == NULL){
+		val = -1;
+		printf("Linked List Empty");
+		}
+	else{
+		temp = first;
+		temp_b = NULL;
+		while(temp->next != NULL){
+			temp_b = temp;
+			temp = temp->next;
+			}
+		val = temp->val;
+		temp_b->next = NULL;
+		free(temp);
+		printf("Value from front %d deleted\n", val);
+		}
+	return first;
+	}
+	
+node ins_p(node first, int pos, int val){
+	node temp, curr;
+	int count = 1;
+	if(first == NULL){
+		first = get_node();
+		first-> val = val;
+		}
+	else{
+		curr  = first;
+		while(count != pos-1 && curr->next != NULL){
+			count++;
+			curr = curr->next;
+			}
+				 
+		temp = get_node();
+		temp->val = val;
+		temp->next = (curr->next)->next;
+		curr->next = temp;		
+		}
+	return first;
+	}
+	
+node del_p(node first, int pos){
+	node temp, temp_b;
+	int val, count = 1;
+	if(first == NULL){
+		val = -1;
+		printf("Linked List Empty");
+		}
+	else if(first->next == NULL && pos == 1){
+		val = first->val;
+		printf("Value from front %d deleted\n", val);
+		free(first);
+		first = NULL;
+		}
+	else{
+		temp = first;
+		temp_b = NULL;
+		while(count != pos && temp->next != NULL){
+			count++;
+			temp_b = temp;
+			temp = temp->next;
+			}
+		val = temp->val;
+		temp_b->next = temp->next;
+		free(temp);
+		printf("Value from front %d deleted\n", val);
+		}
+	return first;
+	}
